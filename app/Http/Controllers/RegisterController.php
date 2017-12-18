a19532e1795d6765ece3cae1038f705973bb7e27
RedArr <miao8627@qq.com>
2017-12-18 22:22:01 +0800

    null

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\User;
+use \App\User;
 class RegisterController extends Controller
 {
     public function index()
@@ -12,16 +12,17 @@ class RegisterController extends Controller
     }
     public function register()
     {
-        $this->vaildate(\request(),[
-            'name'=>'required|min:3|unique:user,name',
-            'email'=>'required|unique:users:email|email',
+//        dd(request()->all());
+        $this->validate(request(),[
+            'name'=>'required|min:3|unique:users,name',
+            'email'=>'required|unique:users,email|email',
             'password'=>'required|min:6|max:16|confirmed',
         ]);
+        $name = request('name');
+        $email = request('email');
+        $password = bcrypt(request('password'));
 
-        $name = \request('name');
-        $email = \request('email');
-        $password = \bcrypt(\request('password'));
-        $user = User::create('name','email','password');
+        $user = User::create(compact('name','email','password'));
 
         return redirect('/login');
     }
